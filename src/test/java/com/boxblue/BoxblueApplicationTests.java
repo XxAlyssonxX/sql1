f92ecf278f599015f9a1b804c36053a49b04f72a
XxAlyssonxX <132359941+XxAlyssonxX@users.noreply.github.com>
2024-05-23 14:23:43 -0300

    232

---
+++ src/test/java/com/boxblue/BoxblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxblueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
